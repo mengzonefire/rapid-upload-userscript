9bba46d288262ab80f81f634964b4cda731578bb
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2021-08-22 18:02:20 +0800

    Update loader.tsx

--- src/baidu/legacyPage/loader.tsx
+++ src/baidu/legacyPage/loader.tsx
@@ -11,7 +11,7 @@ function fakeRequire(module: string) {
     try {
       moduleHook();
     } catch (e) {
-      console.error("%s: 执行 %s hook 时发生错误: %s", TAG, e.message);
+      console.error(`${TAG}: 执行 ${module} hook 时发生错误: ${e.message}`);
       console.trace(e);
     }
     hooks.delete(module);
@@ -27,21 +27,12 @@ export function hook(module: any, fn: any) {
   hooks.set(module, fn);
 }
 
-if (window.require) {
-  console.warn("%s 覆盖方式安装，若无效请强制刷新。", TAG);
-  oRequire = window.require;
-  window.require = fakeRequire;
-  Object.assign(fakeRequire, oRequire);
-} else {
-  console.info("%s 钩子方式安装，若失效请报告。", TAG);
-  Object.defineProperty(window, "require", {
-    set(require) {
-      oRequire = require;
-    },
-    get() {
-      return fakeRequire;
-    },
-  });
-}
-
-export default null;
+console.info("%s 钩子方式安装，若失效请前往脚本页反馈", TAG);
+Object.defineProperty(window, "require", {
+  set(require) {
+    oRequire = require;
+  },
+  get() {
+    return fakeRequire;
+  },
+});
