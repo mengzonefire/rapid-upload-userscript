f21dd9dd624c1735939f994c01ac08c23fa45610
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2021-08-23 20:27:08 +0800

    update

--- src/common/const.tsx
+++ src/common/const.tsx
@@ -24,8 +24,5 @@ export const enum appError {
   missSwalCss = "样式包加载失败, 弹出跨域访问窗口请选择允许",
 }
 
-export const styleText = `style='width: 100%;height: 34px;display: block;line-height: 34px;text-align: center;'`;
-export const styleLink = `style='color: #09AAFF;'`;
-export const styleBtn = `style='font-size: 15px;color: #09AAFF;border: 2px solid #C3EAFF;border-radius: 4px;padding: 10px;margin: 0 5px;padding-top: 5px;padding-bottom: 5px; cursor: pointer'`;
-export const htmlDonate = `<p id="bdcode_donate" ${styleText}>若喜欢该脚本, 可前往 <a ${styleLink} href="https://afdian.net/@mengzonefire" rel="noopener noreferrer" target="_blank">赞助页</a> 支持作者<a id="kill_donate" ${styleBtn}><span style="width: auto;">不再显示</span></a></p>`;
-export const htmlFeedback = `<p id="bdcode_feedback" ${styleText}>若有任何疑问, 可前往 <a ${styleLink} href="https://greasyfork.org/zh-CN/scripts/424574" rel="noopener noreferrer" target="_blank">脚本页</a> 反馈<a id="kill_feedback" ${styleBtn}><span class="text" style="width: auto;">不再显示</span></a></p>`;
+export const htmlDonate = `<p id="bdcode_donate" class="mzf_text">若喜欢该脚本, 可前往 <a class="mzf_link" href="https://afdian.net/@mengzonefire" rel="noopener noreferrer" target="_blank">赞助页</a> 支持作者<a id="kill_donate" class="mzf_btn">不再显示</a></p>`;
+export const htmlFeedback = `<p id="bdcode_feedback" class="mzf_text">若有任何疑问, 可前往 <a class="mzf_link" href="https://greasyfork.org/zh-CN/scripts/424574" rel="noopener noreferrer" target="_blank">脚本页</a> 反馈<a id="kill_feedback" class="mzf_btn">不再显示</a></p>`;
