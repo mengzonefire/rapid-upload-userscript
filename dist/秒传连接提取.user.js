1ed3a59fc398c96eea96148883bf78fcbdd8a2fa
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2021-09-02 00:29:30 +0800

    修复新版界面下按钮样式异常

--- dist/秒传连接提取.user.js
+++ dist/秒传连接提取.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name 秒传链接提取
-// @version 2.0.5
+// @version 2.0.6
 // @author mengzonefire
 // @description 用于提取和生成百度网盘秒传链接
 // @homepage https://greasyfork.org/zh-CN/scripts/424574
@@ -957,8 +957,7 @@
         var htmlTagNew = "div.nd-file-list-toolbar__actions";
         var htmlTaglegacy = "div.tcuLAu";
         var htmlTag2legacy = "#h5Input0";
-        var htmlBtnRapidNew = '<a id="bdlink_btn" style="margin-left: 8px;" class="nd-upload-button upload-wrapper"><span class="nd-common-btn nd-file-list-toolbar-action-item u-btn u-btn--primary u-btn--medium u-btn--default is-has-icon"><i class="iconfont inline-block-v-middle icon-copy"></i><span class="inline-block-v-middle nd-file-list-toolbar-action-item-text">秒传</span></span> </a>';
-        var htmlBtnGenNew = '<button id="gen_bdlink_btn" class="u-btn nd-common-btn nd-file-list-toolbar-action-item is-need-left-sep u-btn--normal u-btn--medium u-btn--default is-has-icon"> <span><i class="iconfont inline-block-v-middle nd-file-list-toolbar__action-item-icon icon-share"></i><span class="inline-block-v-middle nd-file-list-toolbar-action-item-text">生成秒传</span></span> </button>';
+        var htmlBtnRapidNew = '<button id="bdlink_btn" class="u-btn nd-file-list-toolbar-action-item u-btn--primary u-btn--default u-btn--small is-round is-has-icon"><i class="iconfont icon-copy"></i><span>秒传</span></button>';
         var htmlBtnRapidlegacy = '<a class="g-button g-button-blue" id="bdlink_btn" title="秒传链接" style="display: inline-block;""><span class="g-button-right"><em class="icon icon-disk" title="秒传链接提取"></em><span class="text" style="width: auto;">秒传链接</span></span></a>';
         var htmlBtnGenlegacy = '<a class="g-button" id="gen_bdlink_btn"><span class="g-button-right"><em class="icon icon-share"></em><span class="text" style="width: auto;">生成秒传</span></span></a>';
         function baiduErrno(errno) {
