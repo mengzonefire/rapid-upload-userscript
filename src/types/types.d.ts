a9fe15cf9dc47c1a30ca7752f14538b8ca0fccd5
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2021-09-06 22:38:27 +0800

    Update types.d.ts

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,13 +1,7 @@
 export {};
 declare global {
   interface Window {
-    define: any;
-    manifest: any;
     require: any;
-  }
-  interface IfakeRequire {
-    (module: string): any;
-    async: any;
   }
   interface String {
     fromBase64: any;
