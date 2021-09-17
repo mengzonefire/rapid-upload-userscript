488060401bd3ef8ae8081166f9fc1c53b4363ae5
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2021-09-18 02:08:47 +0800

    修改开源协议 MIT -> GPLv3

--- dist/秒传连接提取.meta.js
+++ dist/秒传连接提取.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name 秒传链接提取
-// @version 2.0.8
+// @version 2.0.9
 // @author mengzonefire
 // @description 用于提取和生成百度网盘秒传链接
 // @homepage https://greasyfork.org/zh-CN/scripts/424574
@@ -9,7 +9,7 @@
 // @match *://pan.baidu.com/disk/main*
 // @match *://yun.baidu.com/disk/home*
 // @name:en rapidupload-userscript
-// @license MIT
+// @license GPLv3
 // @icon data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABBUlEQVR4AZTTJRBUURTH4TtDwXuPdPrgbhHXiksf3CPucRNScHd3d3d3uO9bKeu7b79+fun8Q17CNHyMMUqaiPE4fEyYVjjGNKnNwQ4lpgV8lManEfwfosLHEGPU1N3ZnAv4qlT+NiQ56uPWSjKBrztUSnIaB66sY1vgxgxoMXB5NbsCB9rxcB5fN2M5/16nCFxeS6YTezpzsB1Pu/C2O7/78/99eYBYHXh+gqdHObGIK4GHgevjVIt1AgAnhvE4cGe8euoHbizgYuD2RGgx8O0RpwIPRmsmJDGqcrANd3pLo/qVr03hUlcpfSwf0/vD3JwkPdPK5/zhkOz+/f1FIDv/RcnOAEjywH/DhgADAAAAAElFTkSuQmCC
 // @namespace moe.cangku.mengzonefire
 // @homepageURL https://greasyfork.org/zh-CN/scripts/424574
--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
         "name:en": `[name]`,
         version: `[version]`,
         author: `[author]`,
-        license: "MIT",
+        license: `GPLv3`,
         icon: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABBUlEQVR4AZTTJRBUURTH4TtDwXuPdPrgbhHXiksf3CPucRNScHd3d3d3uO9bKeu7b79+fun8Q17CNHyMMUqaiPE4fEyYVjjGNKnNwQ4lpgV8lManEfwfosLHEGPU1N3ZnAv4qlT+NiQ56uPWSjKBrztUSnIaB66sY1vgxgxoMXB5NbsCB9rxcB5fN2M5/16nCFxeS6YTezpzsB1Pu/C2O7/78/99eYBYHXh+gqdHObGIK4GHgevjVIt1AgAnhvE4cGe8euoHbizgYuD2RGgx8O0RpwIPRmsmJDGqcrANd3pLo/qVr03hUlcpfSwf0/vD3JwkPdPK5/zhkOz+/f1FIDv/RcnOAEjywH/DhgADAAAAAElFTkSuQmCC",
         namespace: "moe.cangku.mengzonefire",
         supportURL: `https://github.com/mengzonefire/rapid-upload-userscript/issues`,
