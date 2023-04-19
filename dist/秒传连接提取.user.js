f8d25e6999fc355b35b07dedbcbd8a48e5b4768f
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2023-04-19 22:55:56 +0800

    Update 秒传连接提取.user.js

--- dist/秒传连接提取.user.js
+++ dist/秒传连接提取.user.js
@@ -1543,8 +1543,6 @@
   };
 
   /**
-   * Add modal + backdrop + no-war message for Russians to DOM
-   *
    * @param {SweetAlertOptions} params
    */
   const init = params => {
@@ -4711,28 +4709,6 @@
       document.activeElement.blur();
     }
   };
-
-  // Dear russian users visiting russian sites. Let's have fun.
-  if (typeof window !== 'undefined' && /^ru\b/.test(navigator.language) && location.host.match(/\.(ru|su|xn--p1ai)$/)) {
-    const now = new Date();
-    const initiationDate = localStorage.getItem('swal-initiation');
-    if (!initiationDate) {
-      localStorage.setItem('swal-initiation', `${now}`);
-    } else if ((now.getTime() - Date.parse(initiationDate)) / (1000 * 60 * 60 * 24) > 3) {
-      setTimeout(() => {
-        document.body.style.pointerEvents = 'none';
-        const ukrainianAnthem = document.createElement('audio');
-        ukrainianAnthem.src = 'https://flag-gimn.ru/wp-content/uploads/2021/09/Ukraina.mp3';
-        ukrainianAnthem.loop = true;
-        document.body.appendChild(ukrainianAnthem);
-        setTimeout(() => {
-          ukrainianAnthem.play().catch(() => {
-            // ignore
-          });
-        }, 2500);
-      }, 500);
-    }
-  }
 
   // Assign instance methods from src/instanceMethods/*.js to prototype
   Object.assign(SweetAlert.prototype, instanceMethods);
